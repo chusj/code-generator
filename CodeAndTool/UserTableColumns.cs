70f8241cfb2551bd32940454ed8ef5cbbbaff07a
chusj <chusj.mail@qq.com>
2024-01-30 14:08:30 +0800

    oracle库对应的表和字段的基本信息实体

---
+++ CodeAndTool/UserTableColumns.cs
@@ -0,0 +1,60 @@
+﻿namespace CodeAndTool
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class UserTableColumns
+	{
+		/// <summary>
+		/// 表名
+		/// </summary>
+        public string table_name { get; set; }
+
+		/// <summary>
+		/// 列名
+		/// </summary>
+		public string column_name { get; set; }
+
+		/// <summary>
+		/// 数据类型
+		/// </summary>
+		public string data_type { get; set; }
+
+		/// <summary>
+		/// 数据长度
+		/// </summary>
+		public string data_length { get; set; }
+
+		/// <summary>
+		/// 精度
+		/// </summary>
+		public string data_precision { get; set; }
+
+		/// <summary>
+		/// 小数点位
+		/// </summary>
+		public string data_scale { get; set; }
+
+		/// <summary>
+		/// 默认值
+		/// </summary>
+		public object data_default { get; set; }
+
+		/// <summary>
+		/// 列id
+		/// </summary>
+		public int column_id { get; set; }
+
+		/// <summary>
+		/// 注释
+		/// </summary>
+		public string comments { get; set; }
+
+		/*
+		 select table_name,column_name,data_type,data_length,data_precision,data_scale,data_default,column_id,
+(select comments from user_col_comments b where a.table_name = b.table_name and a.column_name = b.column_name) as comments
+ from user_tab_columns a where table_name = 'SMS_ACCOUNT'
+ order by a.column_id;
+		 */
+	}
+}
---
+++ CodeAndTool/UserTables.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAndTool
+{
+	/// <summary>
+	/// 用户表，对应视图：user_tables
+	/// </summary>
+	public class UserTables
+	{
+		/// <summary>
+		/// 表名
+		/// </summary>
+		public string table_name { get; set; }
+
+		/// <summary>
+		/// 注释
+		/// </summary>
+		public string comments { get; set; }
+
+		/// <summary>
+		/// 行数
+		/// </summary>
+		public string num_rows { get; set; }
+
+		/// <summary>
+		/// 列数
+		/// </summary>
+		public string num_columns { get; set;}
+
+
+		/*
+		 select table_name,num_rows,
+		(select count(0) from user_tab_columns b where a.table_name = b.table_name) as num_columns,
+		(select comments from user_tab_comments c where a.table_name = c.table_name) as comments 
+		from user_tables a where table_name = 'SMS_ACCOUNT';
+		order by a.table_name
+		 */
+	}
+}
