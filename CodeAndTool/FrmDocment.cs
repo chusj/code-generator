e09f67e03874e403a09bb817302d2b3ecfb90e7a
chusj <chusj.mail@qq.com>
2024-02-02 17:46:11 +0800

    some code

--- CodeAndTool/FrmDocment.cs
+++ CodeAndTool/FrmDocment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SqlSugar;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,6 +23,18 @@ namespace CodeAndTool
             txbConnstr.Text = connStr;
         }
 
+        private SqlSugarClient InitDb()
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                DbType = SqlSugar.DbType.Oracle,
+                ConnectionString = ConnStr,
+                IsAutoCloseConnection = true
+            });
+
+            return db;
+        }
+
         private void btnWord_Click(object sender, EventArgs e)
         {
 
@@ -40,6 +53,18 @@ namespace CodeAndTool
         private void btnMarkdown_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void SearchTableInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(" SELECT b.table_name,a.comments");
+            sb.Append(" FROM user_tab_comments a,user_tables b where a.table_name = b.table_name");
+            
+            sb.Append(" order by a.table_name");
+            
+
+            DataTable dt = InitDb().Ado.GetDataTable(sb.ToString());
         }
     }
 }
