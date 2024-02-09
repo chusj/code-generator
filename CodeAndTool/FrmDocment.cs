bdc3ac2a8a15bf1291eb6bd9a93f7c99edb39066
chusj <chusj.mail@qq.com>
2024-02-09 22:38:53 +0800

    开始编写，输出markdown内容，换行有点问题

--- CodeAndTool/FrmDocment.cs
+++ CodeAndTool/FrmDocment.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -52,19 +53,76 @@ namespace CodeAndTool
 
         private void btnMarkdown_Click(object sender, EventArgs e)
         {
+            string path = ConfigurationManager.AppSettings["FileSavePath"];
+
+            string content = AppendMarkdown(SearchTableInfo());
+
+            CreateFile(path, content);
 
         }
 
-        private void SearchTableInfo()
+        private string AppendMarkdown(List<UserTables> tables)
         {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (UserTables table in tables)
+            {
+                sb.Append("### ");
+                sb.Append(table.table_name);
+                sb.Append("    ");
+                sb.Append("#### ");
+                sb.Append(table.comments);
+                sb.Append("    ");
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 创建Sql文件
+        /// </summary>
+        /// <param name="diskPath"></param>
+        /// <param name="content">内容</param>
+        private void CreateFile(string diskPath, string content)
+        {
+            string fileName = DateTime.Now.ToString("yyyy-MM-dd") + ".md"; //处理文件名
+            string filePath = Path.Combine(diskPath, fileName);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(content);
+            }
+        }
+
+        private List<UserTables> SearchTableInfo()
+        {
+            List<UserTables> tables = new List<UserTables>();
+
             StringBuilder sb = new StringBuilder();
             sb.Append(" SELECT b.table_name,a.comments");
             sb.Append(" FROM user_tab_comments a,user_tables b where a.table_name = b.table_name");
-            
             sb.Append(" order by a.table_name");
-            
+
 
             DataTable dt = InitDb().Ado.GetDataTable(sb.ToString());
+
+            int i = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                if(i > 10)
+                {
+                    break;
+                }
+                UserTables userTables = new UserTables();
+                userTables.table_name = dr[0].ToString();
+                userTables.comments = dr[1].ToString();
+
+                tables.Add(userTables);
+
+                i++;
+            }
+
+            return tables;
         }
     }
 }
