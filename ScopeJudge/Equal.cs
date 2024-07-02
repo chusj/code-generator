e416f1acb37e341e9ef5b99c8288c94b9a890b2c
chusj <chusj.mail@qq.com>
2024-07-02 17:59:43 +0800

    优化；增加等于和不等于判断！

---
+++ ScopeJudge/Equal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ScopeJudge
+{
+    public class Equal
+    {
+        public static bool Equals(decimal actualValue, decimal conditionValue)
+        {
+            return actualValue.Equals(conditionValue);
+        }
+
+        /// <summary>
+        /// 等于
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actualValue"></param>
+        /// <param name="conditionValue"></param>
+        /// <returns></returns>
+        public static bool Equals<T>(T actualValue, T conditionValue) where T : IEquatable<T>
+        {
+            return actualValue.Equals(conditionValue);
+        }
+
+        /// <summary>
+        /// 不等于
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actualValue"></param>
+        /// <param name="conditionValue"></param>
+        /// <returns></returns>
+        public static bool NotEquals<T>(T actualValue, T conditionValue) where T : IEquatable<T>
+        {
+            return !actualValue.Equals(conditionValue);
+        }
+    }
+}
