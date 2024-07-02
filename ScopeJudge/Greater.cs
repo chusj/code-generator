9b9399c44c9f2c2d9c0e7437aafcdd2ab773bf2f
chusj <chusj.mail@qq.com>
2024-07-02 17:39:42 +0800

    小于、小于等于、大于、大于等于

---
+++ ScopeJudge/Greater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScopeJudge
+{
+    internal class Greater
+    {
+        /// <summary>
+        /// 大于
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actualValue">实际值</param>
+        /// <param name="conditionValue">条件值</param>
+        /// <returns></returns>
+        public static bool GreaterThan<T>(T actualValue, T conditionValue) where T : IComparable<T>
+        {
+            return actualValue.CompareTo(conditionValue) > 0;
+        }
+
+        /// <summary>
+        /// 大于等于
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actualValue">实际值</param>
+        /// <param name="conditionValue">条件值</param>
+        /// <returns></returns>
+        public static bool GreatThanOrEqual<T>(T actualValue, T conditionValue) where T : IComparable<T>
+        {
+            return actualValue.CompareTo(conditionValue) >= 0;
+        }
+    }
+}
---
+++ ScopeJudge/Less.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ScopeJudge
+{
+    public  class Less
+    {
+        /// <summary>
+        /// 小于
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actualValue">实际值</param>
+        /// <param name="conditionValue">条件值</param>
+        /// <returns></returns>
+        public static bool LessThan<T>(T actualValue, T conditionValue) where T : IComparable<T>
+        {
+            return actualValue.CompareTo(conditionValue) < 0;
+        }
+
+        /// <summary>
+        /// 小于等于
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actualValue">实际值</param>
+        /// <param name="conditionValue">条件值</param>
+        /// <returns></returns>
+        public static bool LessThanOrEqual<T>(T actualValue, T conditionValue) where T : IComparable<T>
+        {
+            return actualValue.CompareTo(conditionValue) <= 0;
+        }
+
+    }
+}
